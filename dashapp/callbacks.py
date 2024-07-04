870a3ebae2113d35b70c9bdea7fa2f3ffeef4572
TxChar <pontogreen1@gmail.com>
2024-07-04 19:40:24 +0700

    feat: 🎸 Added selection options

--- dashapp/callbacks.py
+++ dashapp/callbacks.py
@@ -9,9 +9,11 @@ def register_callbacks(app):
         Output("output-table", "children"), [Input("provinces-dropdown", "value")]
     )
     def update_table(selected_province):
-        df = (graduated_api()).drop(["pp3year"], axis=1)
+        df = (graduated_api()).drop(["pp3year", "level"], axis=1)
+
         if selected_province:
-            df = df[df["schools_province"] == selected_province]
+            df = df[df["schools_province"].isin(selected_province)]
+
         graduated_df = df.rename(
             columns={
                 "level": "ระดับการศึกษา",
@@ -25,4 +27,16 @@ def register_callbacks(app):
         return dash_table.DataTable(
             columns=[{"name": col, "id": col} for col in graduated_df.columns],
             data=graduated_df.to_dict("records"),
+            style_header={
+                "color": "black",
+                "fontWeight": "bold",
+            },
+            style_cell={"padding-right": "10px", "fontSize": 18},
+            style_data={"color": "black", "backgroundColor": "white"},
+            style_data_conditional=[
+                {
+                    "if": {"row_index": "even"},
+                    "backgroundColor": "rgb(220, 220, 220)",
+                }
+            ],
         )
