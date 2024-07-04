6a78a0dfa07d6ea8cf0fb23dc75646878ec0a42b
TxChar <pontogreen1@gmail.com>
2024-07-04 18:51:10 +0700

    feat: 🎸 Change columns're name and added drop down selection

--- dashapp/callbacks.py
+++ dashapp/callbacks.py
@@ -5,12 +5,24 @@ from api import graduated_api
 
 
 def register_callbacks(app):
-    @app.callback(Output("output-table", "children"), [Input("year-dropdown", "value")])
-    def update_table(selected_year):
-        # filtered_df = df[df["Year"] == selected_year]
-        filtered_df = graduated_api()
+    @app.callback(
+        Output("output-table", "children"), [Input("provinces-dropdown", "value")]
+    )
+    def update_table(selected_province):
+        df = (graduated_api()).drop(["pp3year"], axis=1)
+        if selected_province:
+            df = df[df["schools_province"] == selected_province]
+        graduated_df = df.rename(
+            columns={
+                "level": "ระดับการศึกษา",
+                "schools_province": "จังหวัด",
+                "totalmale": "จำนวนผู้ชาย",
+                "totalfemale": "จำนวนผู้หญิง",
+                "totalstd": "จำนวนทั้งหมด",
+            }
+        )
 
         return dash_table.DataTable(
-            columns=[{"name": col, "id": col} for col in filtered_df.columns],
-            data=filtered_df.to_dict("records"),
+            columns=[{"name": col, "id": col} for col in graduated_df.columns],
+            data=graduated_df.to_dict("records"),
         )
