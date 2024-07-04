786647100ed9d6ae2b502fa0f40ce206883d1d81
TxChar <pontogreen1@gmail.com>
2024-07-04 18:51:54 +0700

    feat: 🎸 created dropdown selection from provinces

--- dashapp/layout.py
+++ dashapp/layout.py
@@ -1,21 +1,21 @@
 from dash import html, dcc
+from api import graduated_api
 import pandas as pd
 
 # เตรียมข้อมูล
-df = pd.DataFrame(
-    {"Year": [2000, 2001, 2002, 2003, 2004], "Value": [10, 15, 13, 17, 20]}
-)
+df = graduated_api()
 
 # สร้าง dropdown options
-dropdown_options = [{"label": str(year), "value": year} for year in df["Year"]]
+dropdown_options = [
+    {"label": str(schools_province), "value": schools_province}
+    for schools_province in df["schools_province"]
+]
 
 # กำหนดเลย์เอาท์ของแอป
 layout = html.Div(
     children=[
-        html.H1(children="Dash: Dropdown Example"),
-        dcc.Dropdown(
-            id="year-dropdown", options=dropdown_options, value=2000  # ค่าเริ่มต้น
-        ),
+        html.H1(children="จำนวนนักเรียนที่สำเร็จการศึกษาประจำปี 2566"),
+        dcc.Dropdown(id="provinces-dropdown", options=dropdown_options),  # ค่าเริ่มต้น
         html.Div(id="output-table"),
     ]
 )
