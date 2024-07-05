7e7293a39fcbb29e4f0e48579240a8d8a564da69
TxCharnom <6410110060@psu.ac.th>
2024-07-05 11:29:40 +0700

    Refactor code

--- dashapp/app.py
+++ dashapp/app.py
@@ -1,18 +1,33 @@
-from dash import Dash
 import dash_bootstrap_components as dbc
+from callbacks.callbacks import register_callbacks
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+from pages import homepage, layout
+
+app = Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[dbc.themes.DARKLY],
+)
+app.title = "GRADUATED STUDENTS DASH"
+
+app.layout = html.Div(
+    [
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="page-content"),
+    ]
+)
 
 
-# สร้างอินสแตนซ์ของแอป
-app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/layout":
+        return layout.layout
+    else:
+        return homepage.layout
 
-# สามารถตั้งค่าบางอย่างเพิ่มเติมได้ที่นี่
-app.config.suppress_callback_exceptions = True
 
 if __name__ == "__main__":
-    from layout import layout
-    from callbacks import register_callbacks
-
-    app.layout = layout
+    server = app.server
     register_callbacks(app)
-
     app.run_server(debug=True)
---
+++ dashapp/callbacks/callbacks.py
@@ -0,0 +1,45 @@
+from callbacks.api import graduated_api
+from dash import dash_table
+from dash.dependencies import Input, Output
+
+
+def table_slection(app):
+    @app.callback(
+        Output("output-table", "children"), [Input("provinces-dropdown", "value")]
+    )
+    def update_table(selected_province):
+        df = (graduated_api()).drop(["pp3year", "level"], axis=1)
+
+        if selected_province:
+            df = df[df["schools_province"].isin(selected_province)]
+
+        graduated_df = df.rename(
+            columns={
+                "level": "ระดับการศึกษา",
+                "schools_province": "จังหวัด",
+                "totalmale": "จำนวนผู้ชาย",
+                "totalfemale": "จำนวนผู้หญิง",
+                "totalstd": "จำนวนทั้งหมด",
+            }
+        )
+
+        return dash_table.DataTable(
+            columns=[{"name": col, "id": col} for col in graduated_df.columns],
+            data=graduated_df.to_dict("records"),
+            style_header={
+                "color": "black",
+                "fontWeight": "bold",
+            },
+            style_cell={"padding-right": "10px", "fontSize": 18},
+            style_data={"color": "black", "backgroundColor": "white"},
+            style_data_conditional=[
+                {
+                    "if": {"row_index": "even"},
+                    "backgroundColor": "rgb(220, 220, 220)",
+                }
+            ],
+        )
+
+
+def register_callbacks(app):
+    table_slection(app)
