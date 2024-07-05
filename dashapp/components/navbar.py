a89dd3beb25e0824a1c202ebe29b85bbae29f577
TxCharnom <6410110060@psu.ac.th>
2024-07-05 11:29:05 +0700

    Created Navbar componetn

---
+++ dashapp/components/navbar.py
@@ -0,0 +1,23 @@
+import dash_bootstrap_components as dbc
+
+
+def Navbar():
+    return dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("หน้าหลัก", href="/")),
+            dbc.NavItem(dbc.NavLink("ตาราง", href="/layout")),
+            dbc.DropdownMenu(
+                children=[
+                    dbc.DropdownMenuItem("กรุณาเลือกหน้า", header=True),
+                    dbc.DropdownMenuItem("ตาราง", href="/layout"),
+                ],
+                nav=True,
+                in_navbar=True,
+                label="อื่น ๆ",
+            ),
+        ],
+        brand="นักเรียนมัธยมศึกษาปีที่6 ที่สำเร็จการศึกษาประจำปี2566",
+        brand_href="/",
+        color="primary",
+        dark=True,
+    )
