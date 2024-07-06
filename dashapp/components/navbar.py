de4f7c1f8dcd93dee8de861f3cab027fe035aae0
TxChar <pontogreen1@gmail.com>
2024-07-06 10:19:00 +0700

    style: 💄 change navbar style

--- dashapp/components/navbar.py
+++ dashapp/components/navbar.py
@@ -1,24 +1,61 @@
 import dash_bootstrap_components as dbc
+from dash import callback, html, Input, Output, dash_table, dcc
+
+
+# def Navbar():
+#     return dbc.NavbarSimple(
+#         children=[
+#             dbc.NavItem(dbc.NavLink("หน้าหลัก", href="/")),
+#             dbc.NavItem(dbc.NavLink("ตาราง", href="/layout")),
+#             dbc.DropdownMenu(
+#                 children=[
+#                     dbc.DropdownMenuItem("กรุณาเลือกหน้า", header=True),
+#                     dbc.DropdownMenuItem("ตาราง", href="/layout"),
+#                 ],
+#                 nav=True,
+#                 in_navbar=True,
+#                 label="อื่น ๆ",
+#             ),
+#         ],
+#         brand="แดชบอร์ด นักเรียนมัธยมศึกษาปีที่6 ที่สำเร็จการศึกษาประจำปี2566",
+#         brand_href="/",
+#         color="primary",
+#         dark=True,
+#         className="navbar",
+#     )
 
 
 def Navbar():
-    return dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink("หน้าหลัก", href="/")),
-            dbc.NavItem(dbc.NavLink("ตาราง", href="/layout")),
-            dbc.DropdownMenu(
+    return html.Div(
+        [
+            dbc.NavbarSimple(
                 children=[
-                    dbc.DropdownMenuItem("กรุณาเลือกหน้า", header=True),
-                    dbc.DropdownMenuItem("ตาราง", href="/layout"),
+                    dbc.NavItem(
+                        dbc.Button(
+                            dbc.NavLink("หน้าหลัก", href="/"),
+                            color="primary",
+                            className="ms-2",
+                            n_clicks=0,
+                        )
+                    ),
+                    dbc.NavItem(
+                        dbc.Button(
+                            dbc.NavLink(
+                                "ตาราง",
+                                href="/layout",
+                            ),
+                            color="primary",
+                            className="ms-2",
+                            n_clicks=0,
+                            id="planet_reports_button",
+                        )
+                    ),
                 ],
-                nav=True,
-                in_navbar=True,
-                label="อื่น ๆ",
+                brand="แดชบอร์ด นักเรียนมัธยมศึกษาปีที่6 ที่สำเร็จการศึกษาประจำปี2566",
+                brand_style={"fontSize": 20},
+                brand_href="/",
+                color="dark",
+                dark=True,
             ),
-        ],
-        brand="แดชบอร์ด นักเรียนมัธยมศึกษาปีที่6 ที่สำเร็จการศึกษาประจำปี2566",
-        brand_href="/",
-        color="primary",
-        dark=True,
-        className="navbar",
+        ]
     )
