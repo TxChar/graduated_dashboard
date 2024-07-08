ceaf3194ddfc01955f1da1971fd1075d93b23678
TxChar <pontogreen1@gmail.com>
2024-07-08 19:21:04 +0700

    style: 💄 Change navbar css

--- dashapp/components/navbar.py
+++ dashapp/components/navbar.py
@@ -2,29 +2,6 @@ import dash_bootstrap_components as dbc
 from dash import callback, html, Input, Output, dash_table, dcc
 
 
-# def Navbar():
-#     return dbc.NavbarSimple(
-#         children=[
-#             dbc.NavItem(dbc.NavLink("หน้าหลัก", href="/")),
-#             dbc.NavItem(dbc.NavLink("ตาราง", href="/layout")),
-#             dbc.DropdownMenu(
-#                 children=[
-#                     dbc.DropdownMenuItem("กรุณาเลือกหน้า", header=True),
-#                     dbc.DropdownMenuItem("ตาราง", href="/layout"),
-#                 ],
-#                 nav=True,
-#                 in_navbar=True,
-#                 label="อื่น ๆ",
-#             ),
-#         ],
-#         brand="แดชบอร์ด นักเรียนมัธยมศึกษาปีที่6 ที่สำเร็จการศึกษาประจำปี2566",
-#         brand_href="/",
-#         color="primary",
-#         dark=True,
-#         className="navbar",
-#     )
-
-
 def Navbar():
     return html.Div(
         [
@@ -41,8 +18,8 @@ def Navbar():
                     dbc.NavItem(
                         dbc.Button(
                             dbc.NavLink(
-                                "ตาราง",
-                                href="/layout",
+                                "เกี่ยวกับ",
+                                href="/about",
                             ),
                             color="primary",
                             className="ms-2",
